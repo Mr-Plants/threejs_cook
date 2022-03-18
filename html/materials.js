d29e6980471979ceda70ef16275532d5126e25e4
weiyabo1 <weiyabo1@jd.com>
2022-03-18 11:28:40 +0800

    导入模型

---
+++ html/materials.js
@@ -0,0 +1,134 @@
+export const materials = {
+    "soft_mid5_default2": {
+        "roughness": 0.2,
+        "metalness": 1,
+        "color": "#c4cdd9",
+        "emissive": "#929cc3",
+        "opacity": 1,
+        "transparent": true
+    },
+    "soft_dark_default2": {
+        "roughness": 0.36,
+        "metalness": 1,
+        "color": "#70a2ff",
+        "emissive": "#315cdd",
+        "opacity": 1,
+        "transparent": true
+    },
+    "soft_light1_default2": {
+        "roughness": 0.2,
+        "metalness": 1,
+        "color": "#c4cdd9",
+        "emissive": "#a2acd8",
+        "opacity": 1,
+        "transparent": true
+    },
+    "soft_light2_default2": {
+        "roughness": 0.2,
+        "metalness": 1,
+        "color": "#c4cdd9",
+        "emissive": "#7b83a3",
+        "opacity": 1,
+        "transparent": true
+    },
+    "soft_mid1_default2": {
+        "roughness": 0.2,
+        "metalness": 1,
+        "color": "#8d99aa",
+        "emissive": "#7d88ba",
+        "opacity": 0.99,
+        "transparent": true
+    },
+    "soft_mid2_default2": {
+        "roughness": 0.2,
+        "metalness": 1,
+        "color": "#c4cdd9",
+        "emissive": "#7b83a3",
+        "opacity": 1,
+        "transparent": true
+    },
+    "soft_light3_default2": {
+        "roughness": 0.2,
+        "metalness": 1,
+        "color": "#8d99aa",
+        "emissive": "#7d88ba",
+        "opacity": 0.99,
+        "transparent": true
+    },
+    "soft_light4_default2": {
+        "roughness": 0.2,
+        "metalness": 1,
+        "color": "#c4cdd9",
+        "emissive": "#7b83a3",
+        "opacity": 1,
+        "transparent": true
+    },
+    "soft_light5_default2": {
+        "roughness": 0.2,
+        "metalness": 1,
+        "color": "#8d99aa",
+        "emissive": "#7d88ba",
+        "opacity": 0.99,
+        "transparent": true
+    },
+    "soft_light6_default2": {
+        "roughness": 0.2,
+        "metalness": 1,
+        "color": "#c4cdd9",
+        "emissive": "#9ca5c9",
+        "opacity": 1,
+        "transparent": true
+    },
+    "glass_light_default2": {
+        "frontMaterial": {
+            "side": 0,
+            "transparent": true,
+            "roughness": 0.1,
+            "metalness": 0.2,
+            "transmission": 0.97,
+            "reflectivity": 1,
+            "envMapIntensity": 2,
+            "color": "#d6dcfa"
+        },
+        "backMaterial": {
+            "side": 1,
+            "transparent": true,
+            "roughness": 0.01,
+            "metalness": 0.01,
+            "transmission": 0.71,
+            "reflectivity": 1,
+            "envMapIntensity": 2,
+            "color": "#d6dcfa"
+        }
+    },
+    "glass_mid2_default2": {
+        "frontMaterial": {
+            "side": 0,
+            "transparent": true,
+            "roughness": 0.1,
+            "metalness": 0.2,
+            "transmission": 0.97,
+            "reflectivity": 1,
+            "envMapIntensity": 2,
+            "color": "#f0f2ff"
+        },
+        "backMaterial": {
+            "side": 1,
+            "transparent": true,
+            "roughness": 0.01,
+            "metalness": 0.01,
+            "transmission": 0.71,
+            "reflectivity": 1,
+            "envMapIntensity": 2,
+            "color": "#f0f2ff"
+        }
+    },
+    "soft_light_default2": {
+        "roughness": 0.35,
+        "metalness": 1,
+        "color": "#c4cdd9",
+        "emissive": "#959fc6",
+        "opacity": 1,
+        "transparent": true
+    }
+}
